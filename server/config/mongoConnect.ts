8fea5e745755f5643f2cca0a3d5ee0e994d28a4a
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2022-12-03 02:08:40 +0300

    hotfix mongoose connect

--- server/config/mongoConnect.ts
+++ server/config/mongoConnect.ts
@@ -2,9 +2,7 @@ import * as mongoose from "mongoose";
 
 
 export default async function mongoConnect() {
-	if (process.env.MONGODB_URI)
-		await mongoose.connect(process.env.MONGODB_URI);
-	else
+	if (!process.env.MONGODB_URI)
 		await mongoose.connect("mongodb://localhost:27017/main", {
 			authSource: "admin",
 			user: process.env.MONGO_USERNAME,
