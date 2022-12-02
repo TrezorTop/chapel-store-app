1bad03862decc30e926c2c94c81097664012fef9
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2022-12-03 02:54:39 +0300

    test vercel

--- server/config/mongoConnect.ts
+++ server/config/mongoConnect.ts
@@ -2,16 +2,11 @@ import * as mongoose from "mongoose";
 
 
 export default async function mongoConnect() {
-	if (!process.env.MONGODB_URI)
-		await mongoose.connect("mongodb://localhost:27017/main", {
-			authSource: "admin",
-			user: process.env.MONGO_USERNAME,
-			pass: process.env.MONGO_PASSWORD,
-		});
-	else
-		await mongoose.connect(process.env.MONGODB_URI as string, {
-			authSource: "admin",
-			user: process.env.MONGO_USERNAME,
-			pass: process.env.MONGO_PASSWORD,
-		});
+	let mongoUri = process.env.MONGODB_URI ?? "mongodb://localhost:27017/main";
+
+	await mongoose.connect(mongoUri, {
+		authSource: "admin",
+		user: process.env.MONGO_USERNAME,
+		pass: process.env.MONGO_PASSWORD,
+	});
 }
