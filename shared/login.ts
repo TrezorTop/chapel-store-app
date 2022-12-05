86dfb413a44e0beb04bebd7cf74dae6b10f53316
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2022-12-05 20:48:57 +0300

    add login types

---
+++ shared/login.ts
@@ -0,0 +1,15 @@
+import { AuthBasePath, BasePath } from "./index";
+
+
+export const LoginBasePath = "/login";
+
+export const LoginPath = `${BasePath}${AuthBasePath}${LoginBasePath}`;
+
+export type LoginRequest = {
+	username: string;
+	password: string;
+};
+
+export type LoginResponse = {
+	accessToken: string;
+};
