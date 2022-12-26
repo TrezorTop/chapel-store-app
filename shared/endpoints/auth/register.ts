0e6bfc0b83a7fda609c5e43aaa7d0808fddefba3
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2022-12-26 18:03:08 +0300

    add register validator

--- shared/endpoints/auth/register.ts
+++ shared/endpoints/auth/register.ts
@@ -1,9 +1,23 @@
 import { AuthRootPath, BasePath } from "../../index";
+import { Validator } from "../../types";
 
 
 export const RegisterBasePath = "/register";
 
 export const RegisterPath = `${BasePath}${AuthRootPath}${RegisterBasePath}`;
+
+export const RegisterRequestValidators: Validator<RegisterRequest> = {
+	username: [
+		value => value.length > 8 || "Минимальная длина 8 символов",
+		value => value.length < 32 || "Максимальная длина 32 символа",
+		value => /[a-zA-Z\d_.]/gu.test(value) || "Разрешенные символы: a-Z, A-Z, цифры, _ и .",
+	],
+	password: [
+		value => value.length > 8 || "Минимальная длина 8 символов",
+		value => value.length < 32 || "Максимальная длина 32 символа",
+		value => /[a-zA-Z\d_.!@#$%^&*']/gu.test(value) || "Разрешенные символы: a-Z, A-Z, цифры и '_.!@#$%^&*'",
+	]
+};
 
 export type RegisterRequest = {
 	username: string;
