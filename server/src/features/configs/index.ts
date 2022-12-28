9442ce5db370868dfe3b9e68683552ebb53665a2
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2022-12-28 20:30:25 +0300

    add createConfig

--- server/src/features/configs/index.ts
+++ server/src/features/configs/index.ts
@@ -1,7 +1,9 @@
 import { FastifyPluginAsync } from "fastify/types/plugin";
+import { create } from "./create";
 import { getAll } from "./getAll";
 
 
 export const configsModule: FastifyPluginAsync = async (instance) => {
 	instance.register(getAll);
+	instance.register(create);
 };
