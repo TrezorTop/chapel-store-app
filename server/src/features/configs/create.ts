9a6907ad2d23365eb95ecf1b529434ae31fdf078
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2023-01-11 20:31:12 +0300

    fix mime types

--- server/src/features/configs/create.ts
+++ server/src/features/configs/create.ts
@@ -109,6 +109,8 @@ async function processAsSingleArchiveFile(id: string, file: Required<File>) {
 
 function isArchive(mimeType: string) {
 	return mimeType === "application/zip" ||
+		mimeType === "application/x-rar" ||
+		mimeType === "application/x-7z" ||
 		mimeType === "application/x-rar-compressed" ||
 		mimeType === "application/x-7z-compressed";
 }
