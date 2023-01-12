c7c1baf4011c39ac51f6a66798957d8913034637
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2023-01-12 22:28:47 +0300

    update schema

--- server/src/infrastructure/prismaConnect.ts
+++ server/src/infrastructure/prismaConnect.ts
@@ -5,3 +5,10 @@ export type JsonValue = Prisma.JsonValue
 
 
 export const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+	if (params.action === "findMany") {
+		console.log(42);
+	}
+	return next(params);
+});
