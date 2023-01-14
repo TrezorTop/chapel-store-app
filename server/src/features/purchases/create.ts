5141bf187c16c48975eb2bef41c0cbad0374e8bf
Yura Slobodenyuk <catalan.1ap.main@gmail.com>
2023-01-14 17:53:47 +0300

    1

--- server/src/features/purchases/create.ts
+++ server/src/features/purchases/create.ts
@@ -1,7 +1,9 @@
 import { Role } from "@prisma/client";
+import axios from "axios";
 import cuid from "cuid";
 import { FastifyInstance } from "fastify";
 import { StatusCodes } from "http-status-codes";
+import { CreatePurchases_NotFound } from "../../../../shared/consts/error";
 import {
 	CreatePaymentBasePath,
 	CreatePaymentRequest,
@@ -10,15 +12,19 @@ import {
 import { Validator } from "../../../../shared/types";
 import { jwtOnRequestHook } from "../../infrastructure/jwtConfig";
 import { prisma } from "../../infrastructure/prismaConnect";
+import { cancelIfFailed } from "../../infrastructure/utils";
 import { validatePreValidationHook } from "../../infrastructure/validatePreValidationHook";
 
 
 const bodyValidator: Validator<CreatePaymentRequest> = {
-	configId: {
+	bundleId: {
 		check: [value => cuid.isCuid(value) || "Невалидный id"],
 		required: true
+	},
+	email: {
+		check: [],
+		required: false
 	}
-
 };
 
 
@@ -32,13 +38,38 @@ export const create = async (instance: FastifyInstance) => {
 	}, async (request, reply) => {
 		const body = request.body;
 
-		await prisma.purchases.create({
+		const bundle = await cancelIfFailed(() => prisma.bundle.findUnique({
+				where: {
+					id: body.bundleId
+				}
+			}), StatusCodes.NOT_FOUND, CreatePurchases_NotFound
+		);
+
+		const order = await prisma.uncomittedOrders.create({
 			data: {
 				ownerUsername: request.user.username,
-				configId: body.configId
+				bundleId: bundle.id
+			}
+		});
+		const res = await axios.post<CreateInvoiceResponse>("https://api.cryptocloud.plus/v1/invoice/create", {
+			shop_id: process.env.CRYPTOCLOUD_SHOPID,
+			amount: bundle.price,
+			order_id: order.id,
+			currency: "RUB",
+			...(body.email && { email: body.email })
+		}, {
+			headers: {
+				"Authorization": `Token ${process.env.CRYPTOCLOUD_TOKEN}`
 			}
 		});
 
-		return reply.status(StatusCodes.OK).send({ url: "hueta" });
+		return reply.status(StatusCodes.OK).send({ url: res.data.pay_url });
 	});
 };
+
+type CreateInvoiceResponse = {
+	status: "success" | "error",
+	pay_url: string,
+	invoice_id: string,
+	currency: string
+}
